cadffb40263db8f7bf5bf37c12bf123f4b7a2728
ALOUILATIFA <aloui_latifa@yahoo.fr>
2018-01-10 20:34:17 +0100

    Dossier

---
+++ src/Test_Voiture.java
@@ -0,0 +1,15 @@
+
+public class Test_Voiture {
+
+	public static void main(String[] args) {
+		
+		Voiture V1= new Voiture("BMW", "TWINGO", "124C67", 10000);
+		V1.calcul_prix_reel(12.5);
+		V1.affiche();
+		
+		
+
+		
+	}
+
+}
---
+++ src/Voiture.java
@@ -0,0 +1,24 @@
+
+public class Voiture {
+
+	private String marque;
+	private String modele;
+	private String numero;
+	private double prix_initial, prix_reel;
+	
+	public Voiture (String m, String mo, String n, double p) {
+		this.marque=m;
+		this.modele=mo;
+		this.numero=n;
+		this.prix_initial=p;
+		this.prix_reel=0;
+	}
+	 public void affiche() {
+		System.out.println(marque+" "+modele+" "+numero+" "+prix_initial+" "+prix_reel); 
+	 }
+	public void calcul_prix_reel(double t) {
+		prix_reel=prix_initial*(1-t);
+	}
+	
+	
+}
